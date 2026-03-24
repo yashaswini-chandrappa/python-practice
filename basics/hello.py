e008f8dd5f0c25cf705e33a0e51682cc3b9fe1e4
Yashaswini Chandrappa <97212256+yashaswini-chandrappa@users.noreply.github.com>
2026-03-24 17:13:50 +0530

    Enhance hello.py with input and printing examples
    Added multiple print examples and input handling.

--- basics/hello.py
+++ basics/hello.py
@@ -1,2 +1,24 @@
-##Hello World
+## 1.Hello World
 print("Hello World")
+
+## 2.Printing
+a = input()
+b = input()
+separator = input()[0]
+# Print with space
+print(a+" "+b)
+# Print without newline at the end
+print(a+" "+b,end="")
+# Print with separator
+print(a+separator+b)
+# Print without space
+print(a+b)
+
+## 3. Int str -  Write your code below that prints a a times and b b times, seperated by c
+
+a = int( input())
+b = int(input())
+c = input()
+print(str(a)*a+c+str(b)*b)
+
+
